6af661863e6f9d17e3a0de0ce2c374b5f351ca58
jcansdale-test <60072076+jcansdale-test@users.noreply.github.com>
2020-07-13 11:39:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace effective_spork_public
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var who = args.Length > 0 ? args[0] : "World";
+            Console.WriteLine($"Hello, {who}!");
+        }
+    }
+}
